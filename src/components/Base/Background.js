b0feb530a2fe33f1cb2d25e8109ecafd46f8e546
Farnaz Mohamadzadeh <farnaz.zm87@gmail.com>
2023-04-26 12:50:20 +0430

    css fixed

--- src/components/Base/Background.js
+++ src/components/Base/Background.js
@@ -8,10 +8,10 @@ export const Background = () => {
       <div className="lg:w-24  lg:h-24  bg-blue-600/5 bg-opacity-30 absolute  lg:top-[70rem] lg:right-8 rounded-full pointer-events-none animate-pulse"></div>
       <div className="lg:w-40  lg:h-40  bg-blue-600/5 bg-opacity-30 absolute  lg:top-[75rem] lg:right-32 rounded-full pointer-events-none"></div>
       <div className="lg:w-44  lg:h-44  bg-blue-600/5 bg-opacity-30 absolute  lg:bottom-0 lg:left-2 rounded-full pointer-events-none"></div>
-      <div className="  text-blue-600/5 bg-opacity-30 absolute text-9xl font-extrabold lg:top-[45rem] lg:right-24 pointer-events-none animate-pulse ">
+      <div className=" hidden lg:block text-blue-600/5 bg-opacity-30 absolute text-9xl font-extrabold lg:top-[45rem] lg:right-24 pointer-events-none animate-pulse ">
         WEB
       </div>
-      <div className="  text-blue-600/5 bg-opacity-30 absolute text-5xl font-extrabold lg:top-[52rem] lg:right-24 pointer-events-none">
+      <div className="hidden lg:block  text-blue-600/5 bg-opacity-30 absolute text-5xl font-extrabold lg:top-[52rem] lg:right-24 pointer-events-none">
         DEVELOPER
       </div>
     </>
